61f0cf9d4a60a765bb7742b505af743a4a5a3811
gabrielgandra29 <gabrielgandra29@gmail.com>
2022-11-09 20:56:34 -0300

    Criando questao 2

---
+++ questao_2_ate_8/respostas.js
@@ -0,0 +1,20 @@
+const pessoas = [];
+
+function questao_2()
+{
+    pessoas.push({ nome:"Fabiana Araújo", age: 33 });
+    pessoas.push({ nome:"Gabriel Gomes", age: 25 });
+    pessoas.push({ nome:"Fernando Henrique", age: 17 });
+    pessoas.push({ nome:"Ana Luiza", age: 2 });
+    pessoas.push({ nome:"Geralda do Nascimento", age: 93 });
+    pessoas.push({ nome:"Miguel Souza", age: 70 });
+    pessoas.push({ nome:"Antonio Miguel", age: 69 });
+}
+
+
+
+
+
+
+
+
