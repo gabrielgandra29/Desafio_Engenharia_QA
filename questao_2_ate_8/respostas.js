9cecce36ad09f050c93af5b85fa8d26adbac6fe1
gabrielgandra29 <gabrielgandra29@gmail.com>
2022-11-10 11:20:49 -0300

    Questão 4 realizada

--- questao_2_ate_8/respostas.js
+++ questao_2_ate_8/respostas.js
@@ -1,4 +1,8 @@
+
 const pessoas = [];
+const primeiroNome = [];
+
+
 
 function questao_2()
 {
@@ -9,7 +13,57 @@ function questao_2()
     pessoas.push({ nome:"Geralda do Nascimento", age: 93 });
     pessoas.push({ nome:"Miguel Souza", age: 70 });
     pessoas.push({ nome:"Antonio Miguel", age: 69 });
+
+    
 }
+
+function questao_3()
+{
+    questao_2();
+    const entrada = document.getElementById('entrada_questao_3').value;
+    
+    console.log(verificarNome(entrada));
+
+}
+
+function verificarNome(entrada){
+    for (let index = 0; index < pessoas.length; index++) {
+        
+        if(pessoas[index].nome === entrada){
+            let pessoa = pessoas[index];
+            return pessoa;
+            
+        }
+    }
+}
+
+function questao_4(){
+    questao_2();
+
+    for (let index = 0; index < pessoas.length; index++) {
+        let pessoa = pessoas[index].nome;      
+        let firstName = pessoa.split(" ")[0]; //Foi utilizado o split para capturar somente o  primeiro nome. 
+        primeiroNome.push(firstName);
+    }
+
+    console.log(primeiroNome);
+
+
+}
+
+function questao_5(){
+
+}
+
+function questao_6(){
+    
+}
+
+function questao_7(){
+    
+}
+
+
 
 
 
